f1d5efe1b2fa8fac2b0833f052f7805079972ed1
klitscher <672@holbertonschool.com>
2019-06-24 23:58:41 +0000

    Update file for exercise 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,40 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - creates an infinite loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - creates 5 zombie processes
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, status;
+	pid_t my_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		my_pid = fork();
+		if (my_pid == 0)
+			exit(0);
+		else
+			printf("Zombie process created, PID: %d\n", my_pid);
+	}
+	infinite_while();
+	return (0);
+}
