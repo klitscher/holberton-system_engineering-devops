345ea9fdb774fd5303241da9aeb71dac8e78db3c
klitscher <672@holbertonschool.com>
2019-09-16 19:47:32 +0000

    Update file so its not imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,26 @@
 import requests
 import sys
 
-# Grab data from website
-employee = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-)
-todo = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
-)
-# Convert data to JSON
-employee_dict = employee.json()
-todo_list = todo.json()
-# Save data
-employee_name = employee_dict.get('name')
-number_of_tasks = len(todo_list)
-completed_tasks = [dic for dic in todo_list if dic.get('completed') is True]
-# Print data in format specified
-print('Employee {} is done with tasks({}/{}):'.format(
-    employee_name,
-    len(completed_tasks),
-    number_of_tasks)
-  )
-for task in completed_tasks:
-    print('\t{}'.format(task.get('title')))
+if __name__ == '__main__':
+    # Grab data from website
+    employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    )
+    todo = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
+    )
+    # Convert data to JSON
+    employee_dict = employee.json()
+    todo_list = todo.json()
+    # Save data
+    employee_name = employee_dict.get('name')
+    number_of_tasks = len(todo_list)
+    completed_tasks = [dic for dic in todo_list if dic.get('completed') is True]
+    # Print data in format specified
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employee_name,
+        len(completed_tasks),
+        number_of_tasks)
+      )
+    for task in completed_tasks:
+        print('\t{}'.format(task.get('title')))
