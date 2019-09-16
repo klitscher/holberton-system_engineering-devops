000c70f4f18509393918ab80b199a4e942b3135a
klitscher <672@holbertonschool.com>
2019-09-16 20:08:13 +0000

    Updating file to not be importable

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,28 +6,30 @@ import json
 import requests
 import sys
 
-# Grab data from website
-employee = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-)
-todo = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
-)
-# Convert data to JSON
-employee_dict = employee.json()
-todo_list = todo.json()
-# Save data
-json_data = {}
-task_data = []
-task_new_dict = {}
-for task in todo_list:
-    task_new_dict = {
-        'task': task.get('title'),
-        'completed': task.get('completed'),
-        'username': employee_dict.get('username'),
-    }
-    task_data.append(task_new_dict)
-json_data = {sys.argv[1]: task_data}
-# Export data to json
-with open('{}.json'.format(sys.argv[1]), 'w') as employee:
-    json.dump(json_data, employee)
+
+if __name__ == '__main__':
+    # Grab data from website
+    employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    )
+    todo = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
+    )
+    # Convert data to JSON
+    employee_dict = employee.json()
+    todo_list = todo.json()
+    # Save data
+    json_data = {}
+    task_data = []
+    task_new_dict = {}
+    for task in todo_list:
+        task_new_dict = {
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': employee_dict.get('username'),
+        }
+        task_data.append(task_new_dict)
+    json_data = {sys.argv[1]: task_data}
+    # Export data to json
+    with open('{}.json'.format(sys.argv[1]), 'w') as employee:
+        json.dump(json_data, employee)
