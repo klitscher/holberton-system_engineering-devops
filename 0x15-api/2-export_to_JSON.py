c379132deb80ca45ce79c314f8e92fda40fd8352
klitscher <672@holbertonschool.com>
2019-09-16 19:29:13 +0000

    Adding file for exercise 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module to connect to an api and grab data"""
+
+import csv
+import json
+import requests
+import sys
+
+# Grab data from website
+employee = requests.get(
+    'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+)
+todo = requests.get(
+    'https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]
+)
+# Convert data to JSON
+employee_dict = employee.json()
+todo_list = todo.json()
+# Save data
+json_data = {}
+task_data = []
+task_new_dict = {}
+for task in todo_list:
+    task_new_dict = {
+        'task': task.get('title'),
+        'completed': task.get('completed'),
+        'username': employee_dict.get('username'),
+    }
+    task_data.append(task_new_dict)
+json_data = {sys.argv[1]: task_data}
+# Export data to json
+with open('{}.json'.format(sys.argv[1]), 'w') as employee:
+    json.dump(json_data, employee)
